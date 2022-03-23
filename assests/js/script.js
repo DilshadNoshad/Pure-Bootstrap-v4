829af37f00acd1d986f0442b7fb7e326d3606721
DilshadSheikh <dilshadnoshad2003@gmail.com>
2022-03-23 20:08:23 +0500

    update poet section

--- assests/js/script.js
+++ assests/js/script.js
@@ -11,4 +11,17 @@ $(document).ready(function () {
       $(".gallery").removeClass("change");
     }
   });
+
+  $(".writes-accordion").click(function (event) {
+    if (event.target.id.split("-")[0] === "button") {
+      $("#book-1").attr(
+        "src",
+        "assests/img/" + event.target.id.split("-")[1] + "-book-1.jpg"
+      );
+      $("#book-2").attr(
+        "src",
+        "assests/img/" + event.target.id.split("-")[1] + "-book-2.jpg"
+      );
+    }
+  });
 });
